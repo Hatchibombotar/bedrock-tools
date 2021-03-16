f367c559401edc6a7b6108a5978f1ebe109b1564
Hatchibombotar <ted.hatchibombotar@gmail.com>
2021-03-16 20:42:11 +0000

    Updated Styling and made generation happen when a letter is typed.

--- js/manifest.js
+++ js/manifest.js
@@ -1,11 +1,22 @@
-function download(manifest_name, description, manifest_type, author, url, licence_selection, version_1, version_2, version_3, min_version_1, min_version_2, min_version_3) {
+window.onkeydown = function () {
+    setTimeout(() => {//small pause needed for text to appear
+        createFile()
+    }, 100)
+}
+function createFile() {
+    // var min_version_1 = document.getElementById("file.header.min_engine_version[0]").value
+    // var min_version_2 = document.getElementById("file.header.min_engine_version[1]").value
+    // var min_version_3 = document.getElementById("file.header.min_engine_version[2]").value
+
+    console.log(document.getElementById("file.header.name").value)
     var template = {
         "format_version": 2,
         "header": {
             "name": "",
             "description": "",
             "uuid": "",
-            "version": []
+            "version": [],
+            "min_engine_version": []
         },
         "modules": [
             {
@@ -15,30 +26,38 @@ function download(manifest_name, description, manifest_type, author, url, licenc
             }
         ]
     };
-    template.header.name = manifest_name;
-    template.header.description = description;
+    template.header.name = document.getElementById("file.header.name").value;
+    template.header.description = document.getElementById("file.header.description").value;
     template.header.uuid = createUUID();
     template.modules[0].uuid = createUUID();
-    template.modules[0].type = manifest_type;
-    template.header.version[0] = parseInt(version_1);
-    template.header.version[1] = parseInt(version_2);
-    template.header.version[2] = parseInt(version_3);
-    template.modules[0].version[0] = parseInt(version_1);
-    template.modules[0].version[1] = parseInt(version_2);
-    template.modules[0].version[2] = parseInt(version_3);
-    if (document.getElementById("metadata").checked == true) {
-        template.metadata = { license: licence_selection, url: url, authors: [author] }
+    template.modules[0].type = document.getElementById("file.modules[0].type").value;
+    template.header.version[0] = parseInt(document.getElementById("file.header.version[0]").value);
+    template.header.version[1] = parseInt(document.getElementById("file.header.version[1]").value);
+    template.header.version[2] = parseInt(document.getElementById("file.header.version[2]").value);
+    template.modules[0].version[0] = parseInt(document.getElementById("file.header.version[0]").value);
+    template.modules[0].version[1] = parseInt(document.getElementById("file.header.version[1]").value);
+    template.modules[0].version[2] = parseInt(document.getElementById("file.header.version[2]").value);
+    // template.modules[1].version[0] = parseInt(document.getElementById("file.header.version[0]").value);
+    // template.modules[1].version[1] = parseInt(document.getElementById("file.header.version[1]").value);
+    // template.modules[1].version[2] = parseInt(document.getElementById("file.header.version[2]").value);
+
+    template.header.min_engine_version[0] = parseInt(document.getElementById("file.header.min_engine_version[0]").value);
+    template.header.min_engine_version[1] = parseInt(document.getElementById("file.header.min_engine_version[1]").value);
+    template.header.min_engine_version[2] = parseInt(document.getElementById("file.header.min_engine_version[2]").value);
+
+    if (document.getElementById("toggle.metadata").checked == true) {
+        template.metadata = { license: document.getElementById("file.metadata.license").value, url: document.getElementById("file.metadata.url").value, authors: [document.getElementById("file.metadata.authors").value] }
     }
-    if (manifest_type == "client_data") {
+    if (document.getElementById("file.modules[0].type").value == "client_data") {
         template.modules[1] = { "type": "data", "uuid": createUUID(), "version": [] };
-        template.modules[1].version[0] = parseInt(version_1);
-        template.modules[1].version[1] = parseInt(version_2);
-        template.modules[1].version[2] = parseInt(version_3);
+        template.modules[1].version[0] = parseInt(document.getElementById("file.header.version[0]").value);
+        template.modules[1].version[1] = parseInt(document.getElementById("file.header.version[1]").value);
+        template.modules[1].version[2] = parseInt(document.getElementById("file.header.version[2]").value);
     }
     if (document.getElementById("advanced").checked == true) {
-        if (min_version_1 == 0 && min_version_2 == 0 && min_version_3 == 0) { } else {
-            template.header.min_engine_version = [parseInt(min_version_1), parseInt(min_version_2), parseInt(min_version_3)];
-        }
+        // if (min_version_1 == 0 && min_version_2 == 0 && min_version_3 == 0) { } else {
+        //     template.header.min_engine_version = [parseInt(min_version_1), parseInt(min_version_2), parseInt(min_version_3)];
+        // }
         if (document.getElementById("capabilities").checked == true) {
             if (document.getElementById("worlds_only").checked == true) {
                 template.header.pack_scope = "world";
@@ -58,7 +77,9 @@ function download(manifest_name, description, manifest_type, author, url, licenc
 
     var content = JSON.stringify(template, null, 2);
     document.getElementById('output').innerHTML = content;
-    autosize(document.getElementById('output'));
+    var element = document.getElementById('output');
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
 }
 
 // created a v4 uuid
@@ -82,7 +103,7 @@ function advancedSettings() {
 
 // enables metadata
 function metadataSettings() {
-    var checkBox = document.getElementById("metadata");
+    var checkBox = document.getElementById("toggle.metadata");
     var settings = document.getElementById("metadata_settings");
     if (checkBox.checked == true) {
         settings.style.display = "block";
@@ -114,4 +135,3 @@ function copyText() {
         alert('Unable to copy');
     }
 }
-
